ccf9550a3d90d793ed67fa60a7d878261bd77d6f
Rakshitih <rakshithkumab01718@gmail.com>
2025-10-14 15:23:54 +0530

    Added the UI design for the UserCart  Page

--- src/pages/UserCart.jsx
+++ src/pages/UserCart.jsx
@@ -1,9 +1,119 @@
-import React from 'react'
+import React from "react";
+import CartCards from "../components/CartCards";
 
 const UserCart = () => {
   return (
-    <div>UserCart</div>
-  )
-}
+    <>
+      <section className="bg-white py-8 antialiased dark:bg-gray-900 md:py-16">
+        <div className="mx-auto max-w-screen-xl px-4 2xl:px-0">
+          <h2 className="text-xl font-semibold text-gray-900 dark:text-white sm:text-2xl">
+            Shooping Cart
+          </h2>
 
-export default UserCart
+          <div className="mt-6 sm:mt-8 md:gap-6 lg:flex lg:items-start xl:gap-8">
+            <div className="mx-auto w-full flex-none lg:max-w-2xl xl:max-w-4xl">
+              <div className="space-y-6">
+                <div className="rounded-lg border border-gray-200 bg-white p-4 shadow-sm dark:border-gray-700 dark:bg-gray-800 md;p-6">
+                  <CartCards />
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div className="mx-auto mt-6 max-w-4xl flex-1 space-y-6 lg;mt-0 lg:w-full">
+            <div className="space-y-4 rounded-lg border border-gray-200 bg-white p-4 shadow-sm dark:border-gray-700 dark:bg-gray-800 sm:p-6">
+              <p className="text-xl font-semibold text-gray-900 dark:text-white">
+                Order Summary
+              </p>
+
+              <div className="space-y-4">
+                <div className="space-y-2">
+                  <dl className="flex items-center justify-between gap-4">
+                    <dt className="text-base font-normal text-gray-500 dark:text-gray-400">
+                      Original price
+                    </dt>
+                    <dd className="text-base font-medium text-gray-900 dark:text-white">
+                      $Total Price
+                    </dd>
+                  </dl>
+
+                  <dl className="flex items-center justify-between gap-4">
+                    <dt className="text-base font-normal text-gray-500 dark:text-gray-400">
+                      Savings
+                    </dt>
+                    <dd className="text-base font-medium text-gray-600 ">$0</dd>
+                  </dl>
+
+                  <dl className="flex items-center justify-between gap-4">
+                    <dt className="text-base font-normal text-gray-500 dark:text-gray-400">
+                      Store Pickup
+                    </dt>
+                    <dd className="text-base font-medium text-gray-900 dark:text-white">
+                      $0
+                    </dd>
+                  </dl>
+
+                  <dl className="flex items-center justify-between gap-4">
+                    <dt className="text-base font-normal text-gray-500 dark:text-gray-400">
+                      Tax
+                    </dt>
+                    <dd className="text-base font-medium text-gray-900 dark:text-white">
+                      $0
+                    </dd>
+                  </dl>
+                </div>
+
+                <dl className="flex items-center justify-between gap-4 border-t border-gray-200 pt-2 dark:border-gray-700">
+                  <dt className="text-base font-bold text-gray-900 dark:text-white">
+                    Total
+                  </dt>
+                  <dd className="text-base font-bold text-gray-900 dark:text-white">
+                    $Total price
+                  </dd>
+                </dl>
+              </div>
+
+              <a
+                href="#"
+                className="flex w-full items-center justify-center rounded-lg bg-primary-700 px-5 py-2.5 text-sm font-medium text-white hover:bg-primary-800 focus:outline-none focus:ring-4 focus:ring-primary-300 dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
+              >
+                Proceed to Checkout
+              </a>
+
+              <div className="flex items-center justify-center gap-2">
+                <span className="text-sm font-normal text-gray-500 dark:text-gray-400">
+                  {" "}
+                  or{" "}
+                </span>
+                <a
+                  href="#"
+                  title=""
+                  className="inline-flex items-center gap-2 text-sm font-medium text-primary-700 underline hover:no-underline text-blue-600"
+                >
+                  Continue Shopping
+                  <svg
+                    className="h-5 w-5"
+                    aria-hidden="true"
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                  >
+                    <path
+                      stroke="currentColor"
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                      stroke-width="2"
+                      d="M19 12H5m14 0-4 4m4-4-4-4"
+                    />
+                  </svg>
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    </>
+  );
+};
+
+export default UserCart;
