f7cca03de56bba1284682287ff7b485eac73738c
Rakshitih <rakshithkumab01718@gmail.com>
2025-10-14 14:38:18 +0530

    Created HomeCards Page

--- src/components/HomeCards.jsx
+++ src/components/HomeCards.jsx
@@ -1,8 +1,25 @@
 import React from 'react'
+import logo from "../assets/react.svg"
 
 const HomeCards = () => {
   return (
-    <div>HomeCards</div>
+    <div className='max-w-sm rounded overflow-hidden shadow-lg m-10 '>
+      <img 
+      src={logo}
+      className='w-full' 
+      alt='thumbnails'/>
+      <div className="px-6 py-4">
+        <div className="font-bold text-xl mb-2">Title of the Product
+          <p className='text-gray-700 text-base'>
+          Description of the products
+          </p>
+        </div>
+      
+      <div className="bg-blue-600 text-center h-10 text-lg font-bold flex justify-center hover:bg-blue-500 rounded-md">
+        <button className='cursor-pointer '>Add To Cart</button>
+      </div>
+      </div>
+    </div>
   )
 }
 
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,26 @@
 import React from 'react'
+import HomeCards from '../components/HomeCards'
 
 const Home = () => {
   return (
-    <header className='bg-blue-300 h-100 flex justify-center items-center lg:text-3xl font-bold '>
-      <h1>WelCome To E-Kart</h1>
+    <>
+    <header className='bg-blue-300 h-100 flex justify-center items-center  font-bold '>
+      <h1 className='text-3xl'>WelCome To E-Kart</h1>
     </header>
+    <div className=' flex-row grid grid-cols-4 items-center justify-center'>
+   <HomeCards />
+   <HomeCards />
+   <HomeCards />
+   <HomeCards />
+   <HomeCards />
+   <HomeCards />
+   <HomeCards />
+   <HomeCards />
+   <HomeCards />
+   <HomeCards />
+
+    </div>
+    </>
   )
 }
 
