0610101f4af1ebba48cc0998434175b9d83f894b
Rakshitih <rakshithkumab01718@gmail.com>
2025-10-15 12:31:15 +0530

    Adding Login page UI

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,9 +1,79 @@
-import React from 'react'
+import React from "react";
 
 const Login = () => {
   return (
-    <div>Login</div>
-  )
-}
+    <>
+      <div className="flex h-screen flex-col justify-center px-6 py-12 lg:px-8 bg-gray-600">
+        <div className="sm:mx-auto sm:w-full sm:max-w-sm">
+          <h2 className="mt-10 text-center text-2xl/9 font-bold tracking-tight text-white">
+            Sign in to your account
+          </h2>
+        </div>
 
-export default Login
+        <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm shadow-lg p-3 rounded-md shadow-white-500">
+          <form action="#" method="POST" className="space-y-6">
+            <div>
+              <label
+                for="email"
+                className="block text-sm/6 font-medium text-gray-100"
+              >
+                Email address
+              </label>
+              <div className="mt-2">
+                <input
+                  id="email"
+                  type="text"
+                  name="email"
+                  required
+                  autocomplete="email"
+                  className="block w-full rounded-md bg-white/5 px-3 py-1.5 text-base text-white outline-1 -outline-offset-1 outline-white/10 placeholder:text-gray-500 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-500 sm:text-sm/6"
+                />
+              </div>
+            </div>
+
+            <div>
+              <div className="flex items-center justify-between">
+                <label
+                  for="password"
+                  className="block text-sm/6 font-medium text-gray-100"
+                >
+                  Password
+                </label>
+                <div className="text-sm">
+                  <a
+                    href="#"
+                    className="font-semibold text-indigo-400 hover:text-indigo-300"
+                  >
+                    Forgot password?
+                  </a>
+                </div>
+              </div>
+              <div className="mt-2">
+                <input
+                  id="password"
+                  type="password"
+                  name="password"
+                  required
+                  autocomplete="current-password"
+                  className="block w-full rounded-md bg-white/5 px-3 py-1.5 text-base text-white outline-1 -outline-offset-1 outline-white/10 placeholder:text-gray-500 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-500 sm:text-sm/6"
+                />
+              </div>
+            </div>
+
+            <div>
+              <button
+                type="submit"
+                className="flex w-full justify-center rounded-md bg-indigo-500 px-3 py-1.5 text-sm/6 font-semibold text-white hover:bg-indigo-400 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-500"
+                
+              >
+                Sign in
+              </button>
+            </div>
+          </form>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Login;
